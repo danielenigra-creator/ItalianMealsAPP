bc5b373538da2f39ec9b6d78e92ecc522fc2cc5e
daniele nigra <daniele.nigra@edu-its.it>
2026-06-22 14:36:52 +0200

    Prova 22 giugno

--- ItalianMealsApp/App.tsx
+++ ItalianMealsApp/App.tsx
@@ -1,23 +1,21 @@
-
 import { NavigationContainer } from "@react-navigation/native";
-
-
+import { Linking } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
-import DetailsScreen from "./src/screens/DetailsScreen";
 import { useEffect, useState } from "react";
 import { fetchItalianMeals } from "./src/services/mealsApi";
-import Login from "./src/screens/Login";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Linking } from "react-native";
+
+import LoginScreen from "./src/screens/LoginScreen";
+import MealDetailScreen from "./src/screens/MealDetailScreen";
 const stack = createNativeStackNavigator();
 
 const linking = {
   prefixes: [Linking.createURL("/"), "myapp://"],
   config: {
     screens: {
-      Login: "login",
-      Home: "home",
-      Details: "details/:id",
+      LoginScreen: "login",
+      HomeScreen: "home",
+      MealDetailScreen: "details/:id",
     },
   },
 };
@@ -28,9 +26,9 @@ export default function App() {
   return (
     <NavigationContainer linking={linking}>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Details" component={MealDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
